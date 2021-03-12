c2d8560685da44c6617474a604584bf45c18bc78
a.andriichuk@viseven.com <a.andriichuk@viseven.com>
2021-03-12 11:01:35 +0200

    setState, local state (lesson 71)

--- src/componenents/Profile/ProfileContainer.jsx
+++ src/componenents/Profile/ProfileContainer.jsx
@@ -26,5 +26,5 @@ let mapStateToProps = (state) => ({
 export default compose(
   connect(mapStateToProps, { getUsersProfile }),
   withRouter,
-  withAuthRedirect
+  // withAuthRedirect
 )(ProfileContainer);
--- src/componenents/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/componenents/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,5 +1,6 @@
 import Preloader from "../../common/Preloader/Preloader";
 import s from "./ProfileInfo.module.css";
+import ProfileStatus from "./ProfileStatus";
 const ProfileInfo = (props) => {
   if (!props.profile) {
     return <Preloader />;
@@ -7,12 +8,13 @@ const ProfileInfo = (props) => {
 
   return (
     <div>
-      <img
+      {/* <img
         className="imgResponsive"
         src="https://images.fineartamerica.com/images/artworkimages/mediumlarge/1/sisters-horses-panorama-twenty-two-north-photography.jpg"
-      />
+      /> */}
       <div className={s.descriptionBlock}>
         <img src={props.profile.photos.large} />
+        <ProfileStatus status={"hello my friend!"}/>
         <div>{props.profile.aboutMe}</div>
         <div>{props.profile.lookingForAJobDescription}</div>
       </div>
