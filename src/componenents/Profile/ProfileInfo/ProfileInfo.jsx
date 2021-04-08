edc3bebad838e838cb0a81ed43fe53bdfcd9c0fa
a.andriichuk@viseven.com <a.andriichuk@viseven.com>
2021-04-08 11:07:09 +0300

    hook, useState (lesson 84)

--- src/componenents/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/componenents/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,6 +1,6 @@
 import Preloader from "../../common/Preloader/Preloader";
 import s from "./ProfileInfo.module.css";
-import ProfileStatus from "./ProfileStatus";
+import ProfileStatusWithHooks from "./ProfileStatusWithHooks";
 const ProfileInfo = (props) => {
   if (!props.profile) {
     return <Preloader />;
@@ -14,7 +14,7 @@ const ProfileInfo = (props) => {
       /> */}
       <div className={s.descriptionBlock}>
         <img src={props.profile.photos.large} />
-        <ProfileStatus status={props.status} updateStatus={props.updateStatus}/>
+        <ProfileStatusWithHooks status={props.status} updateStatus={props.updateStatus}/>
         <div>{props.profile.aboutMe}</div>
         <div>{props.profile.lookingForAJobDescription}</div>
       </div>
