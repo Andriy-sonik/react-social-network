7d1c7a8d0a4bd7e074a3d80b0c695dee61806ea2
a.andriichuk@viseven.com <a.andriichuk@viseven.com>
2021-04-26 17:30:38 +0300

    React.lazy and React.Suspense (lesson 94)

--- src/App.js
+++ src/App.js
@@ -1,68 +1,71 @@
-import { Route, withRouter, BrowserRouter } from "react-router-dom";
-import React, { Component } from "react";
-import { connect, Provider } from "react-redux";
-import { compose } from "redux";
-import { initializeApp } from "./redux/app-reducer";
+import {Route, withRouter, BrowserRouter} from "react-router-dom";
+import React, {Component} from "react";
+import {connect, Provider} from "react-redux";
+import {compose} from "redux";
+import {initializeApp} from "./redux/app-reducer";
 import store from "./redux/redux-store";
 import "./App.css";
-import DialogsContainer from "./componenents/Dialogs/DialogsContainer";
 import HeaderContainer from "./componenents/Header/HeaderContainer";
 import Navbar from "./componenents/Navbar/Navbar";
-import ProfileContainer from "./componenents/Profile/ProfileContainer";
 import News from "./componenents/News/News";
 import Music from "./componenents/Music/Music";
 import Setting from "./componenents/Setting/Setting";
 import UsersContainers from "./componenents/Users/UsersContainer";
 import Login from "./componenents/Login/Login";
 import Preloader from "./componenents/common/Preloader/Preloader";
+import {withSuspense} from "./hoc/withSuspense"
+
+const DialogsContainer = React.lazy(() => import("./componenents/Dialogs/DialogsContainer"));
+const ProfileContainer = React.lazy(() => import("./componenents/Profile/ProfileContainer"));
 
 class App extends Component {
-  componentDidMount() {
-    this.props.initializeApp();
-  }
-  render() {
-    if (!this.props.initialized) {
-      return <Preloader />;
-    } else {
-      return (
-        <div className="app-wrapper">
-          <HeaderContainer />
-          <Navbar />
-          <div className="app-wrapper-content">
-            <Route
-              path="/profile/:userId?"
-              render={() => <ProfileContainer />}
-            />
-            <Route path="/dialogs" render={() => <DialogsContainer />} />
-            <Route path="/users" render={() => <UsersContainers />} />
-            <Route path="/login" component={Login} />
-            <Route path="/news" component={News} />
-            <Route path="/music" component={Music} />
-            <Route path="/setting" component={Setting} />
-          </div>
-        </div>
-      );
+    componentDidMount() {
+        this.props.initializeApp();
     }
-  }
+
+    render() {
+        if (!this.props.initialized) {
+            return <Preloader/>;
+        } else {
+            return (
+                <div className="app-wrapper">
+                    <HeaderContainer/>
+                    <Navbar/>
+                    <div className="app-wrapper-content">
+                        <Route
+                            path="/profile/:userId?"
+                            render={withSuspense(ProfileContainer)}
+                        />
+                        <Route path="/dialogs" render={withSuspense(DialogsContainer)}/>
+                        <Route path="/users" render={() => <UsersContainers/>}/>
+                        <Route path="/login" component={Login}/>
+                        <Route path="/news" component={News}/>
+                        <Route path="/music" component={Music}/>
+                        <Route path="/setting" component={Setting}/>
+                    </div>
+                </div>
+            );
+        }
+    }
 }
 
 let mapStateToProps = (state) => ({
-  initialized: state.app.initialized,
+    initialized: state.app.initialized,
 });
 
 let AppContainer = compose(
-  withRouter,
-  connect(mapStateToProps, { initializeApp })
+    withRouter,
+    connect(mapStateToProps, {initializeApp})
 )(App);
 let SamuraiJSApp = () => {
-  return (
-    <React.StrictMode>
-      <BrowserRouter>
-        <Provider store={store}>
-          <AppContainer />
-        </Provider>
-      </BrowserRouter>
-    </React.StrictMode>
-  );
+    return (
+        <React.StrictMode>
+            <BrowserRouter>
+                <Provider store={store}>
+                    <AppContainer/>
+                </Provider>
+            </BrowserRouter>
+        </React.StrictMode>
+    );
 };
 export default SamuraiJSApp;
